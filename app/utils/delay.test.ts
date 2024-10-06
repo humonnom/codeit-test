2c60fd449ae8d44e8e1377c2bf078c208331fcd6
humonnom <juepark42@gmail.com>
2024-10-06 19:04:10 +0900

    비동기 1-3 정답 추가

--- app/utils/delay.test.ts
+++ app/utils/delay.test.ts
@@ -36,6 +36,24 @@ describe("delay", () => {
     }, 64);
   });
 
+  // 1-3. jest fake timer 활용
+  it("should delay `func` execution - jest fake timer", () => {
+    jest.useFakeTimers();
+    const mockCallback = jest.fn();
+
+    delay(mockCallback, 32);
+
+    // 32ms 이전에는 콜백 함수가 호출되지 않아야 합니다.
+    jest.advanceTimersByTime(1);
+    expect(mockCallback).not.toHaveBeenCalled();
+
+    // 64ms 후에는 콜백 함수가 호출되어야 합니다.
+    jest.advanceTimersByTime(64);
+    expect(mockCallback).toHaveBeenCalled();
+
+    jest.useRealTimers(); // 실제시간 사용 (초기화)
+  });
+
   // 2.
   it("should use a default `wait` of `0`", (done) => {
     let pass = false;
